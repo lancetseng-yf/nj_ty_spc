7572eecf8713cb118be7c482fce20e5799455696
Lance Tseng <lancetseng4out@gmail.com>
2025-10-28 20:54:26 +0800

    global language

---
+++ middlewares/i18n.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const i18n = require("i18n");
+
+i18n.configure({
+  locales: ["en", "zh-CN", "zh-TW"],
+  defaultLocale: "en",
+  directory: path.join(__dirname, "../locales"),
+  cookie: "locale",
+  queryParameter: "lang",
+});
+
+function i18nMiddleware(req, res, next) {
+  i18n.init(req, res);
+
+  const userLang =
+    req.cookies?.locale || req.acceptsLanguages(i18n.getLocales()) || "en";
+  i18n.setLocale(req, userLang);
+
+  res.locals.__ = res.__;
+  res.locals.locale = req.getLocale();
+
+  next();
+}
+
+module.exports = { i18nMiddleware, i18n };
--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const sequelize = require("./config/db2");
 const path = require("path");
+const cookieParser = require('cookie-parser');
+const { i18nMiddleware } = require('./middlewares/i18n');
 
+// --- EJS setup ---
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// --- Middleware setup ---
+app.use(cookieParser());
+app.use(i18nMiddleware);
 
 app.use(express.static(path.join(__dirname, "views")));
 app.use("/node_modules", express.static("node_modules"));
 app.use(express.static("public"));
-// Routes
-const indexRoutes = require("./routes/index");
-app.use("/", indexRoutes);
 
+// Routes
+app.use("/", require("./routes/index"));
 app.use("/pps", require("./routes/pps"));
 app.use("/psmax", require("./routes/psmax"));
 app.use("/bmw-kanban", require("./routes/bmw-kanban"));
@@ -23,7 +29,7 @@ app.use("/biscuit", require("./routes/biscuit"));
 app.use("/diecasting-report", require("./routes/diecasting-report"));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT} (v1.32)`);
+  console.log(`Server is running on http://localhost:${PORT} (v1.4)`);
   // Test DB connection
   sequelize
     .authenticate()
